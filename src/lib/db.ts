4338e05c7a32d6f4037f2a0c3017bc6abeb0ee35
Tanaca <jtanacar@gmail.com>
2021-10-13 17:46:15 -0300

    ➕ add mongo, mongoose and post model

---
+++ src/lib/db.ts
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+
+const MONGODB_URI = process.env.MONGODB_URI || "";
+
+let cached = (global as any).mongoose;
+
+if (!cached) {
+    cached = (global as any).mongoose = { conn: null, promise: null };
+}
+
+async function connection() {
+    if (!MONGODB_URI) {
+        throw new Error(
+            "Please define the MONGODB_URI environment variable inside .env.local",
+        );
+    }
+
+    if (cached.conn) {
+        return cached.conn;
+    }
+
+    if (!cached.promise) {
+        const opts = {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            bufferCommands: false,
+        };
+
+        cached.promise = await mongoose
+            .connect(MONGODB_URI, opts)
+            .then((mongoose) => {
+                return mongoose;
+            });
+    }
+    cached.conn = await cached.promise;
+    return cached.conn;
+}
+
+export default connection;
---
+++ src/models/Post.ts
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+import connection from "@lib/db";
+
+const PostSchema = async () => {
+    await connection();
+    return new mongoose.Schema({
+        title: {
+            type: String,
+            required: [true, "É preciso adicionar o titulo do artigo."],
+            maxlength: [20, "O titulo precisa ter no máximo 20 carácter."],
+        },
+        article: {
+            type: String,
+            required: [true, "É necessário adicionar um artigo."],
+        },
+    });
+};
+export default mongoose.models.Post ||
+    mongoose.model("Post", await PostSchema());
